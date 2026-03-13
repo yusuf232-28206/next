47080c069f9d5bc8b2c8cf9a9363fa3ae3f9bf0e
yusuf232-28206 <eirzaq371@gmail.com>
2026-03-13 22:19:26 +0700

    tes2

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,6 @@ export default function Home() {
               width={16}
               height={16}
             />
-            Now
           </a><h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
           </h1>
